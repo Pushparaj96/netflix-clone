a25501585a51b7e6649bb9e4db936714edd7eb04
Pushparaj96 <kpushparaj18@gmail.com>
2024-10-06 13:49:09 +0530

    fix:alignment

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import logo from '../assets/logo.png';
 
 const Header = () => {
   return (
-    <div>
-        <img src={logo} alt="logo" className='h-[148px] w-[200px] left-20 absolute cursor-pointer z-30'/>
+    <div className='relative'>
+        <img src={logo} alt="logo" className='h-[148px] w-[200px] ms-10'/>
     </div>
   )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -16,8 +16,8 @@ const toggleForm = () => {
     <div className='login-background h-screen relative'>
         <div className='absolute inset-0 bg-black bg-opacity-50'></div>
         <Header/>
-         <div className='relative h-full flex justify-center items-center'>
-            <div className='bg-black text-white bg-opacity-60 p-8 rounded-xl w-3/12'>
+         <div className='relative mt-8'>
+            <div className='bg-black text-white bg-opacity-60 p-8 rounded-xl w-3/12 form-container mx-auto right-0 left-0'>
                     <div className='flex gap-1 items-center pb-5'>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7 mt-1">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
