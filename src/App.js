14191ac87915d97cc534363ae6be4bfbc603a2e2
Pushparaj96 <kpushparaj18@gmail.com>
2024-10-28 00:32:44 +0530

    fix: changed routers on App component level

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import Body from "./components/Body";
+import { createBrowserRouter , RouterProvider} from 'react-router-dom';
+import Login from './components/Login';
+import Browse from './components/Browse';
 
 
-function App() {
-  return (<Body/>);
+const App = () => {
+
+  const appRouter = createBrowserRouter([
+    {
+      path:"/",
+      element:<Login/>
+    },
+    {
+      path:"/browse",
+      element:<Browse/>
+    }
+  ])
+
+  return (
+    <RouterProvider router={appRouter} />
+  )
 }
 
 export default App;
