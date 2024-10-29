fa3acbb3896ce139e949220a3ef4b55f6fbd92eb
Pushparaj96 <kpushparaj18@gmail.com>
2024-10-29 16:19:52 +0530

    fetching Movies from TMDB and adding to redux store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,24 @@
 import Header from './Header';
+import { TMDB_OPTIONS } from "../utils/constants";
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addNowPlayingMovies } from '../utils/moviesSlice';
 
 const Browse = () => {
+
+    const dispatch = useDispatch();
+
+    const getNowPlayingMovies = async ()=>{
+        const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', TMDB_OPTIONS);
+        const data = await response.json();
+        console.log(data.results);
+         dispatch(addNowPlayingMovies(data.results));
+    }
+
+    useEffect(()=>{
+        getNowPlayingMovies();
+    },[])
+
     return (
         <div>
             <Header/>
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const TMDB_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTg2MTIzMjYzNWMyNDIzNTc2YmMxODU0NGRkYjFiZCIsIm5iZiI6MTczMDE5NjkxNC4zNzc4NTIsInN1YiI6IjY3MjBiM2M5NzY5MTA3ZDc3YjQ4NzkyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.k1k38rySG6elQdT_ZtwEEV8guTU29EPOk-E4RXnD6nI'
+  }
+};
+
