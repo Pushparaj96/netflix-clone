b2df273d6a358baeb73c69150955add3d072e8f5
Pushparaj96 <kpushparaj18@gmail.com>
2024-10-30 19:45:13 +0530

    conditional rendering all categories

--- src/components/MovieCategory.js
+++ src/components/MovieCategory.js
@@ -1,17 +1,17 @@
 import MovieCard from "./MovieCard";
 
 
-const MovieCategory = ({category,nowPlayingMovies}) => {
+const MovieCategory = ({categoryName,categoryMovies}) => {
 
 
   return (
     <div>
         <div className="py-6">
-            <h2 className="text-2xl">{category}</h2>
+            <h2 className="text-2xl">{categoryName}</h2>
         </div>
         <div className="flex overflow-x-scroll gap-x-5">
             {
-                nowPlayingMovies.map(movie=>(
+                categoryMovies.map(movie=>(
                     <MovieCard key={movie.id} posterPath = {movie.poster_path} movieName = {movie.title} />
                 ))
             }
--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -4,16 +4,29 @@ import { useSelector } from "react-redux";
 
 const SecondaryContainer = () => {
 
-  const nowPlaying = useSelector(store=>store?.movies?.nowPlayingMovies);
+  const nowPlayingMovies = useSelector(store=>store?.movies?.nowPlayingMovies);
+  const popularMovies = useSelector(store=>store?.movies?.popularMovies);
+  const topRatedMovies = useSelector(store=>store?.movies?.topratedMovies);
+  const upcomingMovies = useSelector(store=>store?.movies?.upcomingMovies);
 
 
   return (
     <div className="bg-black text-white">
-      { nowPlaying &&
         <div className="pl-14 -mt-72 z-[200] relative">
-        <MovieCategory category={"Now Playing"} nowPlayingMovies ={nowPlaying} />
+        { nowPlayingMovies &&
+          <MovieCategory categoryName={"Now Playing"} categoryMovies ={nowPlayingMovies} />
+        }
+        { topRatedMovies &&
+            <MovieCategory categoryName={"Top Rated"} categoryMovies ={topRatedMovies} />
+        }
+        { upcomingMovies &&
+            <MovieCategory categoryName={"Upcoming"} categoryMovies ={upcomingMovies} />
+        }
+        { popularMovies &&
+            <MovieCategory categoryName={"Popular Movies"} categoryMovies ={popularMovies} />
+        }
+        
         </div>
-      }
     </div>
   )
 }
