726a272f0681d02cb7a29af53b0cba2ea2907b88
Pushparaj96 <kpushparaj18@gmail.com>
2024-10-30 19:55:38 +0530

    fix: disabled horizontal scroll bar

--- src/components/MovieCategory.js
+++ src/components/MovieCategory.js
@@ -9,7 +9,7 @@ const MovieCategory = ({categoryName,categoryMovies}) => {
         <div className="py-6">
             <h2 className="text-2xl">{categoryName}</h2>
         </div>
-        <div className="flex overflow-x-scroll gap-x-5">
+        <div className="flex overflow-x-scroll gap-x-5 no-scrollbar">
             {
                 categoryMovies.map(movie=>(
                     <MovieCard key={movie.id} posterPath = {movie.poster_path} movieName = {movie.title} />
